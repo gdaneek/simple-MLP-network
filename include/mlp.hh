1be7b021f194b11688686d3cf285f4d6d64933ff
gdaneek <daniladgubazov@gmail.com>
2024-05-18 12:53:06 +0000

    backprop method added

--- include/mlp.hh
+++ include/mlp.hh
@@ -15,11 +15,12 @@
 #include <set>
 #include <cstdlib>
 #include <ctime>
-
+#include <unordered_set>
 typedef double weight;
 typedef std::vector<std::vector<weight>> weight_matrix;
 typedef std::vector<weight> weight_vector;
-
+typedef double nvalue;
+typedef std::vector<nvalue> nvalues_vector;
 /**
  * A structure that implements neural connections between two successive layers <br>
  * Structure is quite simplified because it is assumed that it is only necessary to create a certain number of weights for successive layers <br>
@@ -60,45 +61,76 @@ class NeuralLink {
  *
  */           
 class Neuron {                                                                                                                                          
-    double value;   //< Accumulated value during feedforward
+    nvalue value;   //< Accumulated value during feedforward
   public:
-    double shift;   //< Accumulated value offset. Trainable
+    nvalue shift;   //< Accumulated value offset. Trainable
     /**
       * Neuron constructor
       * \param value the value with which to create a neuron
     */
-    Neuron(double value = 0);              
-    Neuron() = delete;                    
+    Neuron(nvalue value = 0);                           
     /**
       * Sets an attribute `value` to a new value
       * \param value new attribute value
       * \return link to the current object
     */                    
-    Neuron& operator =(double value);           
-     /**
+    Neuron& operator =(nvalue value);           
+    /**
       * Adds any value to the `value` attribute
       * \param value value to be added
       * \return link to the current object
     */                                                     
-    Neuron& operator +=(double value);   
+    Neuron& operator +=(nvalue value);   
     /**
       * returns the current accumulated value
       * \return `value` attribute
     */            
-    double get_value();
-};                                                                            
+    nvalue get_value();
+};                   
+
+/**
+ * Structure implementing a layer of neurons <br>
+ * Neurons are collected in a dynamic array using vector <br>
+ * After creating a layer, the number of neurons cannot be changed <br>
+ */                                                                 
 class Layer {                                                                  
-    std::vector<Neuron> neurons;
+    std::vector<Neuron> neurons;  //< vector of layer's neurons
   public:                                              
-    Layer(size_t size);              
-    Layer(std::vector<Neuron>& neurons);    
-    Layer(weight_vector& neuron_values);                                      
-    weight_vector operator *(weight_vector& weights);               
-                             
-    Layer& operator = (weight_vector values);
-    // тут надо Layer& operator = (weight_vector& values); и Layer& operator = (weight_vector&& values);
-    void apply_offsets();                    
-    size_t size();                                                           
+    /**
+      * Layer constructor <br> 
+      * Creates vector of neurons of size `size` with a default value <br> 
+      * \param size vector size
+    */             
+    Layer(size_t size);            
+    /**
+      * Layer constructor <br> 
+      * Creates a layer from a vector of neurons from another layer <br> 
+      * \param neurons vector of neurons
+    */           
+    Layer(std::vector<Neuron>& neurons);  
+    /**
+      * Layer constructor <br> 
+      * Creates a vector of neurons from a vector of values ​​that need to be set by default <br> 
+      * \param neuron_values vector of neurons
+    */             
+    Layer(nvalues_vector& neuron_values);                                          
+    /**
+      * Allows you to change the values ​​of neurons in a layer
+      * \param values new neuron's values
+      * \return this
+    */       
+    Layer& operator = (nvalues_vector&& values);
+    void apply_offsets();             
+    /**
+      * Calculates the layer size
+      * \return neuron vector size
+    */       
+    size_t size();   
+    /**
+      * Allows you to get a neuron by index <br>
+      * Supports negative indexing
+      * \return neuron with `i` index
+    */                                                               
     Neuron& operator [](int64_t i);                                           
     std::vector<Neuron>::iterator begin();                                    
     std::vector<Neuron>::iterator end();      
@@ -113,56 +145,63 @@ namespace activations {
       friend double derivative(Neuron &neuron, ActivationFunc& f, double accuracy);                                     
       virtual void operator ()(Neuron& neuron) = 0;  
     public:                
-      virtual void operator ()(Layer& layer) = 0;                      
+      virtual void operator ()(Layer& layer) = 0;     
+      virtual std::string name() = 0;                 
   };                                                                         
   class Empty : public ActivationFunc {                                     
       void operator ()(Neuron& neuron) override;        
     public:                 
-      void operator ()(Layer& layer) override;                            
+      void operator ()(Layer& layer) override;  
+      std::string name() override;                          
   };                                                                          
   class ReLU: public ActivationFunc {                                        
       void operator ()(Neuron& neuron) override;        
     public:                 
-      void operator ()(Layer& layer) override;                                       
+      void operator ()(Layer& layer) override;  
+      std::string name() override;                                        
   };                                                                        
   class Tanh: public ActivationFunc {                                        
       void operator ()(Neuron& neuron) override;        
     public:                 
-      void operator ()(Layer& layer) override;                             
+      void operator ()(Layer& layer) override;      
+      std::string name() override;                          
   };                                                                        
   class Sigmoid: public ActivationFunc {                                        
       void operator ()(Neuron& neuron) override;        
     public:                 
-      void operator ()(Layer& layer) override;                           
+      void operator ()(Layer& layer) override;    
+      std::string name() override;                          
   };                                                                         
   class Softmax : public ActivationFunc {                                   
       double exp_sum;                                                          
       void operator ()(Neuron& neuron) override;        
     public:                 
-      void operator ()(Layer& layer) override;                           
+      void operator ()(Layer& layer) override;      
+      std::string name() override;                        
   };                                
 
   double derivative(double x0, ActivationFunc& f, double accuracy);        // должны быть френдами класса активации                                               
   double derivative(Neuron &neuron, ActivationFunc& f, double accuracy);     
-};                                                                             
+};                     
+typedef activations::ActivationFunc* activation_ptr;                                       
                                 
 class Net {                                                                    
     size_t layer_indexer; 
-    std::set<std::tuple<size_t, size_t, activations::ActivationFunc*>> table;
+    std::set<std::tuple<size_t, size_t, activation_ptr>> table;
     void set_input(weight_vector& values);           
     void calc_output();
-    std::tuple<size_t, double> result();     
+    std::tuple<size_t, nvalue> result();     
   public:
     std::vector<Layer> layers;                                                  
-    std::vector<NeuralLink> NeuralLinks;                                        
-    std::vector<activations::ActivationFunc*> activations;                      
-    void add(size_t neuron_count, activations::ActivationFunc* activation, size_t layer_index = 0); 
-    Net(size_t input_size, size_t output_size, activations::ActivationFunc* activation_in, activations::ActivationFunc* activation_out);
-    //Net() = default;
+    std::vector<NeuralLink> links;                                        
+    std::vector<activation_ptr> activations;                      
+    void add(size_t neuron_count, activation_ptr activation, size_t layer_index = 0); 
+    Net(size_t input_size, size_t output_size, activation_ptr activation_in, activation_ptr activation_out);
     void make();                                                               
     size_t size();                                                              
     Layer& operator [](int64_t i);                                              
-    std::tuple<size_t, double> feedforward(weight_vector& input_values);             
+    std::tuple<size_t, nvalue> feedforward(weight_vector& input_values);           
+    void backprop();  
     std::vector<Layer>::iterator begin();                                       
     std::vector<Layer>::iterator end();
     std::vector<Layer>::reverse_iterator rbegin();                                       
--- src/mlp.cc
+++ src/mlp.cc
@@ -1,10 +1,11 @@
 #include "../include/mlp.hh"
 #include </usr/include/SFML/Graphics.hpp>
+
 NeuralLink::NeuralLink(size_t input_layer_size, size_t output_layer_size) {
-    weights = weight_matrix(input_layer_size,weight_vector(output_layer_size));
+    weights = weight_matrix(input_layer_size, weight_vector(output_layer_size));
     for(size_t i{0};i < input_layer_size;i++)
         for(size_t j{0};j < output_layer_size;j++)
-            weights[i][j] = (double)(rand())/RAND_MAX;
+            weights[i][j] = (weight)(rand())/RAND_MAX;
 }
 size_t NeuralLink::size() {return weights.size();}
 weight_vector& NeuralLink::operator [](int64_t i) {return weights[(i < 0)? weights.size()+i : i];  }
@@ -15,49 +16,33 @@ weight_matrix::reverse_iterator NeuralLink::rend() {return weights.rend();}
 
 
 
-Neuron::Neuron(double value) {
-        this->shift = (double)(rand())/RAND_MAX;
-        std::cout <<"shift = " << shift << std::endl;
+Neuron::Neuron(nvalue value) {
+        shift = (nvalue)(rand())/RAND_MAX;
         this->value = value;
 }
-Neuron& Neuron::operator =(double value) {
+Neuron& Neuron::operator =(nvalue value) {
         this->value = value;
         return *this;
 }
-Neuron& Neuron::operator +=(double value) {
+Neuron& Neuron::operator +=(nvalue value) {
     this->value += value;
     return *this;
 }
-double Neuron::get_value() {return value;}
+nvalue Neuron::get_value() {return value;}
 
 
 Layer::Layer(size_t size) {neurons = std::vector<Neuron>(size, Neuron(0));}
 Layer::Layer(std::vector<Neuron>& neurons) {this->neurons = neurons;}
-Layer::Layer(weight_vector& neuron_values) {
-    for(auto value : neuron_values)
-        neurons.push_back(Neuron(value));
-}                                      
+Layer::Layer(nvalues_vector& neuron_values) {for(auto value : neuron_values)neurons.push_back(Neuron(value));}                                      
 
-weight_vector Layer::operator *(weight_vector& weight) {
-    weight_vector mul(neurons.size());
-    for(size_t i{0};i < neurons.size();i++)
-        mul[i] = weight[i]*neurons[i].get_value();
-    return mul;
-}
-
-Layer& Layer::operator =(weight_vector values) {   
+Layer& Layer::operator =(nvalues_vector&& values) {   
     if(values.size() != neurons.size())
         throw std::runtime_error{"The size of the layer and the size of the value vector do not match"};
-    for(size_t i{0}; i < values.size();i++)
-        neurons[i] = values[i];
+    for(size_t i{0}; i < values.size();neurons[i] = values[i], i++);
     return *this;  
 }
 
-void Layer::apply_offsets() {
-    for(size_t i{0};i < neurons.size();i++)
-        neurons[i] = neurons[i].get_value() + neurons[i].shift;
-}
-
+void Layer::apply_offsets() {for(Neuron& neuron : neurons)neuron += neuron.shift;}
 size_t Layer::size() {return neurons.size();}
 Neuron& Layer::operator [](int64_t i) {return neurons[(i < 0)? neurons.size()+i : i];}
 std::vector<Neuron>::iterator Layer::begin() {return neurons.begin();}
@@ -78,13 +63,14 @@ double activations::derivative(Neuron &neuron, ActivationFunc& f, double accurac
    
 void activations::Empty::operator ()(Neuron& neuron) {}
 void activations::Empty::operator ()(Layer& layer) {}
-
+std::string activations::Empty::name(){return "empty";}
 void  activations::ReLU::operator ()(Neuron& neuron) { 
     if(neuron.get_value() < 0)neuron = 0;
 }
 void  activations::ReLU::operator ()(Layer& layer)   { 
     for(Neuron& neuron : layer)(*this)(neuron);
 }
+std::string activations::ReLU::name(){return "ReLU";}
 
 void activations::Tanh::operator ()(Neuron& neuron){
     neuron = std::tanh(neuron.get_value());
@@ -92,6 +78,7 @@ void activations::Tanh::operator ()(Neuron& neuron){
 void activations::Tanh::operator ()(Layer& layer)  {
     for(Neuron& neuron : layer)(*this)(neuron);
 }
+std::string activations::Tanh::name(){return "tanh";}
 
 void activations::Sigmoid::operator ()(Neuron& neuron){
     neuron = 1.0/(1.0+std::exp(-1.0*neuron.get_value())); 
@@ -99,6 +86,7 @@ void activations::Sigmoid::operator ()(Neuron& neuron){
 void activations::Sigmoid::operator ()(Layer& layer)  {
     for(Neuron& neuron : layer)(*this)(neuron);
 }
+std::string activations::Sigmoid::name(){return "sigmoid";}
 
 void  activations::Softmax::operator ()(Neuron& neuron) {
     neuron = std::exp(neuron.get_value())/exp_sum;
@@ -108,12 +96,13 @@ void activations::Softmax::operator ()(Layer& layer)    {
      for(Neuron& neuron : layer)exp_sum += std::exp(neuron.get_value());
      for(Neuron& neuron : layer)(*this)(neuron);
 }
-         
-void Net::add(size_t neuron_count, activations::ActivationFunc* activation, size_t layer_index) {
+std::string activations::Softmax::name(){return "softmax";}
+
+void Net::add(size_t neuron_count, activation_ptr activation, size_t layer_index) {
    table.insert({((layer_index)? layer_index : ++layer_indexer), neuron_count, activation});
 }
 
-Net::Net(size_t input_size, size_t output_size, activations::ActivationFunc* activation_in, activations::ActivationFunc* activation_out) {
+Net::Net(size_t input_size, size_t output_size, activation_ptr activation_in, activation_ptr activation_out) {
     layer_indexer = 0;  
     add(input_size, activation_in, ++layer_indexer);
     add(output_size, activation_out, -1);
@@ -122,12 +111,15 @@ Net::Net(size_t input_size, size_t output_size, activations::ActivationFunc* act
 void Net::make() {
     if(!table.size())return;
     size_t prev_layer_size{0};
+    std::unordered_set<size_t> indexes;
     for(auto& row : table) {
-        // тут надо ошибку если такой индекс уже был
+        if(indexes.count(std::get<0>(row)))
+            throw std::runtime_error{"Network creation error: several different layers have the same index"};
+        else indexes.insert(std::get<0>(row));
         layers.push_back(std::get<1>(row));
         activations.push_back(std::get<2>(row));
         if(prev_layer_size) 
-            NeuralLinks.push_back(NeuralLink(prev_layer_size, (*--layers.end()).size()));
+            links.push_back(NeuralLink(prev_layer_size, (*--layers.end()).size()));
         prev_layer_size = (*--layers.end()).size();
     }
     table.clear();
@@ -149,19 +141,19 @@ weight_vector operator*(Layer& layer, NeuralLink& links) {
 void Net::set_input(weight_vector& values) {layers[0] = values;}
 void Net::calc_output() {
     for(size_t i{1};i < layers.size();i++) {
-        layers[i] = layers[i-1]*NeuralLinks[i-1];
+        layers[i] = layers[i-1]*links[i-1];
         layers[i].apply_offsets();
         (*activations[i])(layers[i]);
     }
 }
-std::tuple<size_t, double> Net::result() {      
-    std::tuple<size_t, double> neuron {0, (*--layers.end())[0].get_value()};
+std::tuple<size_t, nvalue> Net::result() {      
+    std::tuple<size_t, nvalue> neuron {0, (*--layers.end())[0].get_value()};
     for(size_t i{0};i < (*--layers.end()).size();i++) 
-        if(std::get<double>(neuron) < (*--layers.end())[i].get_value()) 
+        if(std::get<nvalue>(neuron) < (*--layers.end())[i].get_value()) 
             neuron = {i, (*--layers.end())[i].get_value()};
     return neuron;
 }
-std::tuple<size_t, double> Net::feedforward(weight_vector& input_values) {
+std::tuple<size_t, nvalue> Net::feedforward(weight_vector& input_values) {
     set_input(input_values);
     calc_output();
     return result();
