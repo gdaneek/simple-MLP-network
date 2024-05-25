383b4d752c57eced1eea6335dcbd7a988a9d726e
gdaneek <daniladgubazov@gmail.com>
2024-05-25 23:48:33 +0000

    initial commit

---
+++ src/console_commands.cc
@@ -0,0 +1,10 @@
+#include "../include/console.hh"
+
+std::map<std::string, void(Console::*)(std::vector<std::string>)> Console::commands {
+    {"save", &Console::save},
+    {"load", &Console::load},
+    {"info", &Console::net_info},
+    {"new", &Console::new_net},
+    {"predict", &Console::predict},
+    {"add", &Console::add_layer}
+};
