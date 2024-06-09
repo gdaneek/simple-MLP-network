1123f4f532aa09141024b1439c1e88870363fd2f
valera-salikhov <valera-salikhov@mail.ru>
2024-06-09 08:12:10 -0400

    add rect, line, trapeze; reworked labels

--- src/dataset.cc
+++ src/dataset.cc
@@ -10,10 +10,14 @@
 #include <tuple>
 #include <filesystem>
 
-using label_t = uint8_t;
+ using label_t = uint8_t;
+// using label_t = char;
+// using label_t = int;
+
 struct Generator {
     size_t img_width, img_height;
     std::unique_ptr<sf::RenderWindow> window_inst;
+    // std::unique_ptr<sf::Texture> window_inst;
     struct Path {
         std::string main;
         std::string train;
@@ -37,13 +41,14 @@ struct Generator {
         return res;
     }
     void save_shape_to_file(sf::Shape& shape, std::string fpath) {
-         window_inst->draw(shape);
-         window_inst->capture().saveToFile(fpath);
+        window_inst->draw(shape);
+        window_inst->capture().saveToFile(fpath);
         window_inst->clear();
     }
     void save_label_to_file(label_t label, std::string fpath) {
         std::ofstream fout(fpath);
-        fout << ((label_t)(1ULL << label));
+        // fout << ((label_t)(1ULL << label));
+        fout << (int)label;
         fout.close();
     }
     void make_lnmap() { // label names map - сопоставляет лейблам строковые названия
@@ -80,7 +85,7 @@ struct Generator {
         for(size_t i{0};i < count;i++) {
 
             auto vertex = std::get<size_t>(*shape_ptr);
-            auto label = std::get<uint8_t>(*shape_ptr);
+            auto label = std::get<label_t>(*shape_ptr);
             size_t rad = (std::rand()%(img_width/2-6))+5;
             sf::CircleShape polygon(rad, vertex); 
             polygon.move(std::rand()%(img_width-rad*2), std::rand()%(img_width-rad*2));
@@ -88,19 +93,77 @@ struct Generator {
             save_label_to_file(label, ((is_train)? path.train : path.test)+"/label_"+std::to_string(i)+"_"+name+".label");
         }
     }
-    void make_rect(size_t count, std::string name, bool is_train = true) {  // suitable for line, rect
-         auto shape_ptr = std::find_if(shape_classes.begin(), shape_classes.end(), [name](std::tuple <std::string, size_t, label_t> elem){return (std::get<std::string>(elem) == name);});
+    void make_rect(size_t count, std::string name, bool is_train = true) {  // suitable for line, rect, rhombus
+        auto shape_ptr = std::find_if(shape_classes.begin(), shape_classes.end(), [name](std::tuple <std::string, size_t, label_t> elem){return (std::get<std::string>(elem) == name);});
 
-        sf::RectangleShape line(sf::Vector2f(150, 1));
         for(size_t i{0};i < count;i++) {
-
             auto vertex = std::get<size_t>(*shape_ptr);
-            auto label = std::get<uint8_t>(*shape_ptr);
-            size_t rad = (std::rand()%(img_width/2-6))+5;
-            sf::CircleShape polygon(rad, vertex); 
-            polygon.move(std::rand()%(img_width-rad*2), std::rand()%(img_width-rad*2));
-            save_shape_to_file(polygon, ((is_train)? path.train : path.test)+"/shape_"+std::to_string(i)+"_"+name+".png");
-            save_label_to_file(label, ((is_train)? path.train : path.test)+"/label_"+std::to_string(i)+"_"+name+".label");
+            auto lable = std::get<uint8_t>(*shape_ptr);
+            if (lable == 0) {
+                int len = std::rand()%img_width + 4;
+                sf::RectangleShape line(sf::Vector2f(len, 1));
+                line.rotate(std::rand()%360);
+                line.move(std::rand()%(img_width-len/2), std::rand()%(img_width-len/2));
+                save_shape_to_file(line, ((is_train)? path.train : path.test)+"/shape_"+std::to_string(i)+"_"+name+".png");
+                save_label_to_file(lable, ((is_train)? path.train : path.test)+"/label_"+std::to_string(i)+"_"+name+".label");
+            } else if (lable == 2){
+                int len = std::rand()%(img_width-3) + 2;
+                sf::RectangleShape rhombus(sf::Vector2f(len, len));
+                rhombus.move(std::rand()%(img_height - len), std::rand()%(img_width - len));
+                // rhombus.rotate(std::rand()%360);
+                save_shape_to_file(rhombus, ((is_train)? path.train : path.test)+"/shape_"+std::to_string(i)+"_"+name+".png");
+                save_label_to_file(lable, ((is_train)? path.train : path.test)+"/label_"+std::to_string(i)+"_"+name+".label");
+            } else if (lable == 3) {
+                int height = std::rand()%(img_height-3) + 2;
+                int width = std::rand()%(img_width-3) + 2;
+                if (height == width) {
+                    height += 1;
+                }
+                sf::RectangleShape rect(sf::Vector2f(height, width));
+                rect.move(std::rand()%(img_height - height), std::rand()%(img_width - width));
+                // rect.move(std::rand()%(img_height), std::rand()%(img_width));
+                // rect.rotate(std::rand()%360);
+                save_shape_to_file(rect, ((is_train)? path.train : path.test)+"/shape_"+std::to_string(i)+"_"+name+".png");
+                save_label_to_file(lable, ((is_train)? path.train : path.test)+"/label_"+std::to_string(i)+"_"+name+".label");
+            }
+        }
+
+    }
+
+    void make_trapeze(size_t count, std::string name, bool is_train = true) {
+        auto shape_ptr = std::find_if(shape_classes.begin(), shape_classes.end(), [name](std::tuple <std::string, size_t, label_t> elem){return (std::get<std::string>(elem) == name);});
+
+        auto vertex = std::get<size_t>(*shape_ptr);
+        auto lable = std::get<uint8_t>(*shape_ptr);       
+
+        for (size_t i{0}; i < count; i++) {
+            sf::ConvexShape trapeze{}; 
+            int height = std::rand()%(img_height-3)+2;
+            int a = std::rand()%(img_width-3)+2;
+            int b = std::rand()%(img_width-3)+2;
+
+            trapeze.setPointCount(4);
+
+            // define the points
+            // trapeze.setPoint(0, sf::Vector2f(0, 0));
+            // trapeze.setPoint(1, sf::Vector2f(a, 0));
+            // trapeze.setPoint(2, sf::Vector2f(b, height));
+            // trapeze.setPoint(3, sf::Vector2f(0, height));
+
+            int start = std::rand()%(img_width/2);
+            int startb = std::rand()%(img_width-b+2)+b+1;
+            trapeze.setPoint(0, sf::Vector2f(start, 0));
+            trapeze.setPoint(1, sf::Vector2f(start + a, 0));
+            trapeze.setPoint(2, sf::Vector2f(startb, height));
+            trapeze.setPoint(3, sf::Vector2f(startb - b, height));
+
+
+            // trapeze.setPoint(4, sf::Vector2f(5, 5));
+
+            trapeze.move(std::rand()%(img_height - std::max(a, b)), std::rand()%(img_width - height));
+
+            save_shape_to_file(trapeze, ((is_train)? path.train : path.test)+"/shape_"+std::to_string(i)+"_"+name+".png");
+            save_label_to_file(lable, ((is_train)? path.train : path.test)+"/label_"+std::to_string(i)+"_"+name+".label");
         }
 
     }
@@ -110,11 +173,20 @@ struct Generator {
 int main() {
     std::srand(std::time(nullptr));
     Generator gen(27,27);
-    std::vector<std::string> shapes{"triangle", "circle", "pentagon"};
+    // std::vector<std::string> shapes{"triangle", "circle", "pentagon"};
+    // std::vector<std::string> shapes{"line", "rectangle", "rhombus", "trapeze"};
+    std::vector<std::string> shapes{"trapeze"};
     for(auto x : shapes)
-        gen.make_shape(30, x);
+        // gen.make_shape(30, x);
+        // gen.make_rect(10, x);
+        gen.make_trapeze(10, x);
 
     for(auto x : shapes)
-        gen.make_shape(5, x, false);
+        // gen.make_shape(5, x, false);
+        // gen.make_rect(10, x, false);
+        gen.make_trapeze(10, x, false);
     return 0;
 }
+
+
+// g++ -o main.exe dataset.cc -lsfml-graphics -lsfml-window -lsfml-system
