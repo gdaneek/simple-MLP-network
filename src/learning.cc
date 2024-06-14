65e50e726ceecc1b05fbd0f568382c3e6385a2b4
valera-salikhov <valera-salikhov@mail.ru>
2024-06-14 14:43:55 -0400

    repush to compile

--- src/learning.cc
+++ src/learning.cc
@@ -146,68 +146,7 @@ std::vector<neuron_t> vectorize_label(LabelType& label) {
 // }
 
 
-// void NetMLP::backprop(const std::vector<double>& target, double learning_rate) {
-//     size_t last = layers.size() - 1;
-//     std::vector<weight> delta_previous(layers.back().size());
-
-//     // double err{0};
-//     // for (size_t j{0}; j < layers.back().size(); j++) {
-//     //     double d_err{(target[j] - layers.back()[j].get_value())}; 
-//     //     err += d_err * d_err;
-//     // }
-//     // err /= 2;
-
-//     for (size_t j{0}; j < layers.back().size(); j++) {
-//         // delta_previous[j] = err * activations::derivative(layers.back()[j], *activations[last], 1e-6);
-//         delta_previous[j] = (target[j] - layers.back()[j].get_value()) * activations::derivative(layers.back()[j], *activations[last], 1e-6);
-//         // delta_previous[j] = (-1) * (target[j] - layers.back()[j].get_value()) * activations::derivative(layers.back()[j], *activations[last], 1e-6);
-//         // delta_previous[j] = (-1) * layers.back()[j].get_value() * (1 - layers.back()[j].get_value()) * (target[j] - layers.back()[j].get_value());
-//     }
-
-//     // last layer shift changes
-//     for (size_t j{0}; j < layers.back().size(); j++) {
-//         layers.back()[j].shift -= delta_previous[j] * learning_rate;
-//     }
-
-
-//     std::vector<weight> delta;
-//     for (int j{last - 1}; j >= 0; j--) {
-//         delta.resize(layers[j].size());
-//         // find d
-//         for (size_t k{0}; k < layers[j].size(); k++) {
-//             double sum_d = 0;
-//             for (size_t t{0}; t < delta_previous.size(); t++) {
-//                 // sum_d += delta_previous[t] * links[j].get_weight(k, t);
-//                 sum_d += delta_previous[t] * links[j].get_weight(t, k);
-//             }
-//             // delta[k] = layers[j][k].get_value() * (1 - layers[j][k].get_value()) * sum_d;
-//             delta[k] = sum_d * activations::derivative(layers[j][k], *activations[j], 1e-6);
-//             // delta[k] = sum_d * ;
-//             // delta[k] = (-1) * sum_d * activations::derivative(layers[j][k], *activations[j], 1e-6);
-//         }
-//         // change w with delta_previous (from k to t) 
-//         for (size_t k{0}; k < layers[j].size(); k++) {
-//             layers[j][k].shift -= delta[k] * learning_rate;
-//             for (size_t t{0}; t < delta_previous.size(); t++) {
-//                 double dw = delta_previous[t] * layers[j][k].get_value() * learning_rate; 
-//                 // weight new_weight = links[j].get_weight(k, t) - dw;
-//                 // links[j].set_weight(k, t, new_weight);
-//                 weight new_weight = links[j].get_weight(t, k) - dw;
-//                 links[j].set_weight(t, k, new_weight);
-//             }
-//         }
-//         // delta_previous = delta
-//         delta_previous.clear();
-//         delta_previous.resize(delta.size());
-//         delta_previous = delta;
-//         delta.clear();
-
-//     }
-// }
-
-
-// for tangh
-void NetMLP::backprop(const std::vector<double>& target, double learning_rate, double moment) {
+void NetMLP::backprop(const std::vector<double>& target, double learning_rate) {
     // calc err---------------------
     double mse{0};
     for (size_t j{0}; j < layers.back().size(); j++) {
@@ -216,14 +155,15 @@ void NetMLP::backprop(const std::vector<double>& target, double learning_rate, d
     mse /= layers.back().size();
     std::cout << mse << std::endl;
     //--------------------------------
-
+    
     size_t last = layers.size() - 1;
     std::vector<weight> delta_previous(layers.back().size());
 
     for (size_t j{0}; j < layers.back().size(); j++) {
-        // delta_previous[j] = (target[j] - layers.back()[j].get_value()) * activations::derivative(layers.back()[j], *activations[last], 1e-6);
-        delta_previous[j] = (-1) * layers.back()[j].get_value() * (1 - layers.back()[j].get_value()) * (target[j] - layers.back()[j].get_value());
-        // delta_previous[j] = layers.back()[j].get_value() * (1 - layers.back()[j].get_value()) * (target[j] - layers.back()[j].get_value());
+        // delta_previous[j] = err * activations::derivative(layers.back()[j], *activations[last], 1e-6);
+        delta_previous[j] = (target[j] - layers.back()[j].get_value()) * activations::derivative(layers.back()[j], *activations[last], 1e-6);
+        // delta_previous[j] = (-1) * (target[j] - layers.back()[j].get_value()) * activations::derivative(layers.back()[j], *activations[last], 1e-6);
+        // delta_previous[j] = (-1) * layers.back()[j].get_value() * (1 - layers.back()[j].get_value()) * (target[j] - layers.back()[j].get_value());
     }
 
     // last layer shift changes
@@ -239,16 +179,21 @@ void NetMLP::backprop(const std::vector<double>& target, double learning_rate, d
         for (size_t k{0}; k < layers[j].size(); k++) {
             double sum_d = 0;
             for (size_t t{0}; t < delta_previous.size(); t++) {
+                // sum_d += delta_previous[t] * links[j].get_weight(k, t);
                 sum_d += delta_previous[t] * links[j].get_weight(t, k);
             }
-            delta[k] = sum_d * (1 - layers[j][k].get_value() * layers[j][k].get_value());
+            // delta[k] = layers[j][k].get_value() * (1 - layers[j][k].get_value()) * sum_d;
+            delta[k] = sum_d * activations::derivative(layers[j][k], *activations[j], 1e-6);
+            // delta[k] = sum_d * ;
+            // delta[k] = (-1) * sum_d * activations::derivative(layers[j][k], *activations[j], 1e-6);
         }
         // change w with delta_previous (from k to t) 
         for (size_t k{0}; k < layers[j].size(); k++) {
             layers[j][k].shift -= delta[k] * learning_rate;
             for (size_t t{0}; t < delta_previous.size(); t++) {
-                double grad{delta_previous[t] * layers[j][k].get_value()};
-                double dw = grad * learning_rate + moment * links[j].get_weight(t, k);
+                double dw = delta_previous[t] * layers[j][k].get_value() * learning_rate; 
+                // weight new_weight = links[j].get_weight(k, t) - dw;
+                // links[j].set_weight(k, t, new_weight);
                 weight new_weight = links[j].get_weight(t, k) - dw;
                 links[j].set_weight(t, k, new_weight);
             }
@@ -261,6 +206,63 @@ void NetMLP::backprop(const std::vector<double>& target, double learning_rate, d
 
     }
 }
+
+
+// // for tangh
+// void NetMLP::backprop(const std::vector<double>& target, double learning_rate, double moment) {
+//     // calc err---------------------
+//     double mse{0};
+//     for (size_t j{0}; j < layers.back().size(); j++) {
+//         mse += (target[j] - layers.back()[j].get_value()) * (target[j] - layers.back()[j].get_value());
+//     }
+//     mse /= layers.back().size();
+//     std::cout << mse << std::endl;
+//     //--------------------------------
+
+//     size_t last = layers.size() - 1;
+//     std::vector<weight> delta_previous(layers.back().size());
+
+//     for (size_t j{0}; j < layers.back().size(); j++) {
+//         delta_previous[j] = (target[j] - layers.back()[j].get_value()) * activations::derivative(layers.back()[j], *activations[last], 1e-6);
+//         // delta_previous[j] = (-1) * layers.back()[j].get_value() * (1 - layers.back()[j].get_value()) * (target[j] - layers.back()[j].get_value());
+//         // delta_previous[j] = layers.back()[j].get_value() * (1 - layers.back()[j].get_value()) * (target[j] - layers.back()[j].get_value());
+//     }
+
+//     // last layer shift changes
+//     for (size_t j{0}; j < layers.back().size(); j++) {
+//         layers.back()[j].shift -= delta_previous[j] * learning_rate;
+//     }
+
+
+//     std::vector<weight> delta;
+//     for (int j{last - 1}; j >= 0; j--) {
+//         delta.resize(layers[j].size());
+//         // find d
+//         for (size_t k{0}; k < layers[j].size(); k++) {
+//             double sum_d = 0;
+//             for (size_t t{0}; t < delta_previous.size(); t++) {
+//                 sum_d += delta_previous[t] * links[j].get_weight(t, k);
+//             }
+//             delta[k] = sum_d * (1 - layers[j][k].get_value() * layers[j][k].get_value());
+//         }
+//         // change w with delta_previous (from k to t) 
+//         for (size_t k{0}; k < layers[j].size(); k++) {
+//             layers[j][k].shift -= delta[k] * learning_rate;
+//             for (size_t t{0}; t < delta_previous.size(); t++) {
+//                 double grad{delta_previous[t] * layers[j][k].get_value()};
+//                 double dw = grad * learning_rate + moment * links[j].get_weight(t, k);
+//                 weight new_weight = links[j].get_weight(t, k) - dw;
+//                 links[j].set_weight(t, k, new_weight);
+//             }
+//         }
+//         // delta_previous = delta
+//         delta_previous.clear();
+//         delta_previous.resize(delta.size());
+//         delta_previous = delta;
+//         delta.clear();
+
+//     }
+// }
 
 
 void train(NetMLP& net, std::string dir) {
@@ -289,7 +291,8 @@ void train(NetMLP& net, std::string dir) {
         auto res = net.feedforward(input);
         std::cout << std::get<size_t>(res) << " " << std::get<neuron_t>(res)<< std::endl;
 
-        net.backprop(target, 0.9, 0.05);
+        // net.backprop(target, 0.9, 0.05);
+        net.backprop(target, 0.05);
 
         //std::cout << *shapes_it << " " << *labels_it << std::endl;
     }
